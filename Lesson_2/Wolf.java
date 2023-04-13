4ddddf4de6f692354f416ca98632bba2669166a9
olgademirtash <olga.demirtash@gmail.com>
2023-04-13 10:22:55 +0300

    Внесены изменения по ДЗ3 (Lesson 2 геттеры-сеттеры) в Wolf.java, WolfTest.java

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,10 +1,54 @@
 public class Wolf {
 
-    String sex;
-    String name;
-    float weight;
-    int age;
-    String color;
+    private String sex;
+    private String name;
+    private float weight;
+    private int age;
+    private String color;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public void go() {
         System.out.println("Идёт");
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,17 +1,18 @@
-
 public class WolfTest {
     public static void main(String[] args) {
         Wolf myWolf = new Wolf();
-        myWolf.sex = "мужской";
-        myWolf.name = "Боб";
-        myWolf.weight = 25.5f;
-        myWolf.age = 10;
-        myWolf.color = "Коричневый";
-        System.out.println("Пол: " + myWolf.sex);
-        System.out.println("Имя: " + myWolf.name);
-        System.out.println("Вес: " + myWolf.weight);
-        System.out.println("Возраст :" + myWolf.age);
-        System.out.println("Цвет: " + myWolf.color);
+        myWolf.setSex("мужской");
+        myWolf.setName("Боб");
+        myWolf.setWeight(25.5f);
+        myWolf.setAge(10);
+        myWolf.setColor("Коричневый");
+
+        System.out.println("Пол: " + myWolf.getSex());
+        System.out.println("Имя: " + myWolf.getName());
+        System.out.println("Вес: " + myWolf.getWeight());
+        System.out.println("Возраст :" + myWolf.getAge());
+        System.out.println("Цвет: " + myWolf.getColor());
+
         myWolf.run();
         myWolf.sit();
         myWolf.go();
