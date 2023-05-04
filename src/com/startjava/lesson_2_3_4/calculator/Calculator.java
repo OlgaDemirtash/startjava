9ab979ebcda90da10b48288db7dc67054a22e148
olgademirtash <olga.demirtash@gmail.com>
2023-05-04 15:46:05 +0300

    Исправления по замечаниям от 03.05.2023

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,16 +1,79 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
-    public static double calculate(int number1, int number2, String sign) {
-        double result = number1;
-        switch (sign) {
-            case "+" -> result = Math.addExact(number1, number2);
-            case "-" -> result = Math.subtractExact(number1, number2);
-            case "*" -> result = Math.multiplyExact(number1, number2);
-            case "/" -> result /= number2;
-            case "^" -> result = Math.pow((double) number1, (double) number2);
-            case "%" -> result %= number2;
+    public static String[] getExpressionElements(String expression) throws Exception {
+        try {
+            return checkExpression(parseExpression(expression));
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
         }
-        return result;
+    }
+
+    public static String parseExpression(String expression) throws Exception {
+        expression = expression.replaceAll("\\s", "");
+        if (expression.contains(".") || expression.contains(",")) {
+            throw new Exception("Допускаются только целые числа и операции  /*-+=%^");
+        }
+        expression = expression.replaceAll("[^ 0-9/*%^+-]", "");
+        if (expression.length() < 3) {
+            throw new Exception("Недопустимая длинна выражения" + expression.length());
+        }
+        expression = expression.replaceAll("[/*%^+-]", " $0 ");
+        expression = expression.replaceAll("\\s+", " ");
+        expression = expression.replaceAll("[/*%^+-](?=-\\d)", " $0 ");
+        expression = expression.replaceAll("(?<=[/*%^+-] )- ", "-");
+        expression = expression.replaceAll("^\s*-\s*(?=\\d)", "-");
+        return expression;
+    }
+
+    public static String[] checkExpression(String expression) throws Exception {
+        String[] exprElements = Arrays.copyOf(expression.trim().split(" "), 3);
+        char pos = '1';
+        if (!isSign(exprElements[1])) {
+            throw new Exception("Вы ввели недопустимый знак " + exprElements[1]);
+        } else {
+            try {
+                isNumber(exprElements[0]);
+                pos = '2';
+                isNumber(exprElements[2]);
+            } catch (Exception e) {
+                throw new Exception((e.getMessage() + "в позицию " + pos));
+            }
+        }
+        return exprElements;
+    }
+
+    private static boolean isSign(String str) {
+        return str.equals("+") || str.equals("-") || str.equals("*")
+                || str.equals("/") || str.equals("^") || str.equals("%");
+    }
+
+    private static void isNumber(String str) throws Exception {
+        try {
+            int number = Integer.parseInt(str);
+            if (number < 0) {
+                throw new Exception("Вы ввели отрицательное число ");
+            }
+        } catch (NumberFormatException e) {
+            throw new Exception("Вы ввели не целое число ");
+        }
+    }
+
+    public static double calculate(String[] Elements) {
+        int number1 = Integer.parseInt(Elements[0]);
+        String sign = Elements[1];
+        int number2 = Integer.parseInt(Elements[2]);
+        return switch (sign) {
+            case "+" -> Math.addExact(number1, number2);
+            case "-" -> Math.subtractExact(number1, number2);
+            case "*" -> Math.multiplyExact(number1, number2);
+            case "/" -> (double) number1 / (double) number2;
+            case "^" -> Math.pow(number1, number2);
+            case "%" -> number1 % number2;
+            default -> 0;
+        };
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,7 +1,6 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 import java.text.DecimalFormat;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class CalculatorTest {
@@ -13,14 +12,31 @@ public class CalculatorTest {
     }
 
     private static void calc() {
-        String[] elementsStr = checkExpression();
-        int[] elementsInt = convertStrArrayToInt(elementsStr);
-        double result = Calculator.calculate(elementsInt[0], elementsInt[1], elementsStr[1]);
-        DecimalFormat dF = new DecimalFormat("#####.###");
-        System.out.println(elementsInt[0] + " " + elementsStr[1] + " " + elementsInt[1] + " = " + dF.format(result));
+        String[] exprElements = inputExpression();
+        double result = Calculator.calculate(exprElements);
+        outputExpression(exprElements, result);
         if (checkAnswer()) {
             calc();
         }
+    }
+
+    private static String[] inputExpression() {
+        Scanner console = new Scanner(System.in);
+        String[] exprElements;
+        System.out.print("Введите математическое выражение: ");
+        String expression = console.nextLine();
+        try {
+            exprElements = Calculator.getExpressionElements(expression);
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + "\n");
+            return inputExpression();
+        }
+        return exprElements;
+    }
+
+    private static void outputExpression(String[] exprElements, double result) {
+        DecimalFormat dF = new DecimalFormat("#####.###");
+        System.out.println(exprElements[0] + " " + exprElements[1] + " " + exprElements[2] + " = " + dF.format(result));
     }
 
     private static boolean checkAnswer() {
@@ -28,83 +44,8 @@ public class CalculatorTest {
         System.out.println("Хотите продолжить вычисления? [yes/no]:");
         String answer = console.nextLine();
         if (!answer.trim().equals(YES) && !answer.trim().equals(NO)) {
-            checkAnswer();
+            return checkAnswer();
         }
         return (answer.trim().equals(YES));
-    }
-
-    private static String[] checkExpression() {
-        String[] elementsStr = Arrays.copyOf(expressionInput().trim().split(" "), 3);
-        if (!isSign(elementsStr[1])) {
-            System.out.printf("Вы ввели недопустимый знак %s, повторите ввод \n", elementsStr[1]);
-            return checkExpression();
-        } else {
-            if (!isNumber(elementsStr[0], 1) || !isNumber(elementsStr[2], 2)) {
-                return checkExpression();
-            }
-        }
-        return elementsStr;
-    }
-
-    private static String expressionInput() {
-        Scanner console = new Scanner(System.in);
-        System.out.print("Введите математическое выражение: ");
-        String expression = console.nextLine();
-        expression = expression.replaceAll("\\s", "");
-        if (expression.contains(".") || expression.contains(",")) {
-            System.out.print("Допускаются только целые числа и операции  /*-+=%^ \n");
-            return expressionInput();
-        }
-        expression = expression.replaceAll("[^ 0-9/*%^+-]", "");
-        if (expression.length() < 3) {
-            System.out.print("Недопустимое выражение \n");
-            return expressionInput();
-        }
-        expression = expression.replaceAll("[/*%^+-]", " $0 ");
-        expression = expression.replaceAll("\\s+", " ");
-        expression = expression.replaceAll("[/*%^+-](?=-\\d)", " $0 ");
-        expression = expression.replaceAll("(?<=[/*%^+-] )- ", "-");
-        return expression;
-    }
-
-    private static boolean isSign(String str) {
-        return str.equals("+") || str.equals("-") || str.equals("*")
-                || str.equals("/") || str.equals("^") || str.equals("%");
-    }
-
-    private static boolean isNumber(String str, int position) {
-        try {
-            int number = Integer.parseInt(str);
-            if (number >= 0) {
-                return true;
-            } else {
-                System.out.printf("Вы ввели отрицательное число в позицию %d, "
-                        + "повторите ввод\n", position);
-            }
-        } catch (NumberFormatException e) {
-            System.out.printf("Вы ввели не целое число в позицию %d, "
-                    + "повторите ввод\n", position);
-        }
-        return false;
-    }
-
-    private static int[] convertStrArrayToInt(String[] strArr) {
-        int lengthIntArr = 0;
-        for (String s : strArr) {
-            if (isSign(s)) {
-                continue;
-            }
-            lengthIntArr++;
-        }
-        int[] intArr = new int[lengthIntArr];
-        int j = 0;
-        for (String s : strArr) {
-            if (isSign(s)) {
-                continue;
-            }
-            intArr[j] = Integer.parseInt(s);
-            j++;
-        }
-        return intArr;
     }
 }
